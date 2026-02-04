5d04af4c119b006489620572d0f4b48f287d4b92
Ben <dev@lumen.invalid>
2026-02-04 19:02:08 +0100

    Create a version 1.4.3 to test on testnet x/upgrade (no changes)

--- app/upgrade_handlers.go
+++ app/upgrade_handlers.go
@@ -23,4 +23,10 @@ func (app *App) RegisterUpgradeHandlers() {
 		// No-op state transition, but still run migrations so module versions are updated.
 		return app.ModuleManager.RunMigrations(ctx, app.Configurator(), fromVM)
 	})
+
+	app.UpgradeKeeper.SetUpgradeHandler("v1.4.3", func(ctx context.Context, plan upgradetypes.Plan, fromVM module.VersionMap) (module.VersionMap, error) {
+		// No-op state transition, but still run migrations so module versions are updated.
+		return app.ModuleManager.RunMigrations(ctx, app.Configurator(), fromVM)
+	})
+
 }
